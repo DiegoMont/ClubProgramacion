21108721761281f543696eb32655481ca246fed6
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2019-01-23 22:17:45 -0600

    Create Jamie.java

---
+++ Jamie.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Jamie {
+  public static void main (String[]args){
+    Scanner sc = new Scanner(System.in);
+    int numCases = Integer.parseInt(sc.nextLine());
+    for (int i = 0; i<numCases ;i++){
+      long resultado = 1L;
+      String [] lectura = new String [3];
+      lectura = sc.nextLine().split(" ");
+      double n = (Double.parseDouble(lectura[0]))/(Double.parseDouble(lectura[1]));
+      long c = (long) Math.floor((n * Math.pow(10, Integer.parseInt(lectura[2]))));
+      for (int j = 1; j<=Integer.parseInt(lectura[2]); j++){
+        if( c%10 != 0){
+          resultado = resultado*(c%10);
+        }
+        c = c/10;
+      }
+      System.out.println(resultado%188888881);
+    }
+  }
+}
