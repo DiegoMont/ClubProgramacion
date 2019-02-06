81950c9cd026be2a3ade998434ef0f05011e99f6
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2019-02-06 12:46:49 -0600

    Multiplicacion de decimales casi terminado

--- Jamie.java
+++ Jamie.java
@@ -4,21 +4,73 @@ import java.io.FileNotFoundException;
 
 class Jamie {
   public static void main (String[]args) throws FileNotFoundException {
+    long currentTime = System.currentTimeMillis();
     Scanner sc = new Scanner(new File("texto.txt"));
-    int numCases = Integer.parseInt(sc.nextLine());
-    for (int i = 0; i<numCases ;i++){
-      long resultado = 1L;
-      String [] lectura = new String [3];
-      lectura = sc.nextLine().split(" ");
-      double n = (Double.parseDouble(lectura[0]))/(Double.parseDouble(lectura[1]));
-      long c = (long) Math.floor((n * Math.pow(10, Integer.parseInt(lectura[2]))));
-      for (int j = 1; j<=Integer.parseInt(lectura[2]); j++){
-        if( c%10 != 0){
-          resultado = resultado*(c%10);
+    int numCases = sc.nextInt();
+    for (int i = 0; i<numCases; i++){
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      long n = sc.nextLong();
+
+
+//for(a = 1; a <= 49;a++){for(b = a+1;b <99;b++){for(n =100;n <= 100;n++){
+
+
+      int residuo = a;
+      int j = 0;
+      long Periodo = 0L;
+      long[] periodo = new long[10];
+      int[] residuos = {a};
+      int resultado = 1;
+      int indexPeriodo = 0;
+      do {
+        residuo *=10;
+        periodo[residuo/b]++;
+        int[] arregloTemp = new int[residuos.length+1];
+        int nuevoResiduo = residuo%b;
+        boolean encontroPeriodo = false;
+        for (int l = 0; l < arregloTemp.length-1; l++) {
+          arregloTemp[l]=residuos[l];
+          if (nuevoResiduo == residuos[l]) {
+            encontroPeriodo=true;
+            indexPeriodo = l;
+          }
         }
-        c = c/10;
+        j++;
+        if (encontroPeriodo) {
+          break;
+        }
+        arregloTemp[residuos.length] = nuevoResiduo;
+        residuos = arregloTemp;
+        residuo = nuevoResiduo;
+      } while (n > j);
+      if(n>j){
+        residuo = residuos[indexPeriodo];
+        long digitosPeriodo =n-j;
+        for (int l = indexPeriodo; l < residuos.length; l++) {
+          residuo*=10;
+          int nuevoPeriodo = residuo/b;
+          if (digitosPeriodo%(residuos.length-indexPeriodo)>0) {
+            periodo[nuevoPeriodo] += digitosPeriodo/(residuos.length-indexPeriodo)+1;
+            digitosPeriodo--;
+          } else {
+            periodo[nuevoPeriodo] += digitosPeriodo/(residuos.length-indexPeriodo);
+          }
+          Periodo= Periodo*10+(nuevoPeriodo==0?1:nuevoPeriodo);
+          residuo%=b;
+        }
       }
-      System.out.println(resultado%188888881);
+      for (int k = 1;k < 10 ; k++) {
+        resultado*=Math.pow(k,periodo[k]);
+        System.out.print(k+": "+periodo[k]+", ");
+      }
+      System.out.println("  a: " + a+"  b: "+b+"  n: "+n+"  "+((double)a/b)+"  Periodo: "+Periodo);
+
+
+//}}}
+
+
     }
+    System.out.println(System.currentTimeMillis() - currentTime+" ms");
   }
 }
